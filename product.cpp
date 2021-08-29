f0cb0007e1e4a1fc9ec9d624d05de6c4ab4a6072
Daksanya Nandhakumar <75685853+Daksanya@users.noreply.github.com>
2021-08-29 10:56:13 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,pro;
+	
+	cout<<"\n Enter first number:";
+	cin>>a;
+	cout<<"\n Enter second number:";
+	cin>>b;
+	
+	cout<<"\n a value:"<<a;
+	cout<<"\n b value:"<<b;
+	
+	pro=a*b;
+	cout<<"\n product is:"<<pro;
+	return 0;
+}
+© 2021 GitHub, Inc.
